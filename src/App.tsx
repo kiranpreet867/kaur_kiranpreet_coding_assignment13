de14fb0c48f69024ae9998f135f29f0aae2b174a
kiranpreet867 <kkaur127@academic.rrc.ca>
2026-07-15 16:14:19 -0500

    bad change

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit page <code>src/App.tsx</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
